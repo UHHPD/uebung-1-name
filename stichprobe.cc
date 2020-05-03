dae62d7230e591b84972574b6e9ade4f37b0f4a0
Kn0wledg3 <64043043+Kn0wledg3@users.noreply.github.com>
2020-05-03 16:27:58 +0200

    Blatt2

---
+++ stichprobe.cc
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+
+double mittelwert (double result, int j){
+  
+  double mitt = 0;
+  
+  mitt = result / j;
+
+  return mitt;
+
+}
+
+double varianz (double result,double x, int j) {
+  
+  double var = 0;
+  double mitt = 0;
+  
+  mitt = result / j;
+  var = (x - mitt) * (x - mitt) / j;
+
+  return var;
+
+}
+
+double standardabweichung (double result, double x, int j){
+
+  double d = 0; 
+  double var = 0;
+  double mitt = 0;
+  
+  mitt = result / j;
+  var = (x - mitt) * (x - mitt) / j;
+  
+  d = sqrt(var);
+
+  return d;
+}
+
+int main() {
+
+std::ifstream eindatei("datensumme.txt");
+
+double x = 0;
+double result = 0;
+double de = 0 ;
+int j = 0;
+
+
+for (int i=0; i < 234; i++){
+  
+  eindatei >> x;
+  result += x;
+  
+  j++;
+
+}
+
+std::cout << "Mittelwert: " << mittelwert (result,j) << std::endl;
+std::cout << "Varianz: " << varianz (result,x,j) << std::endl;
+std::cout << "Standardabweichung: " <<  standardabweichung (result,x,j) << std::endl;
+
+eindatei.close();
+
+}
